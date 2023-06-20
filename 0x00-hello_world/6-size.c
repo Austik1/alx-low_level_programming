e2d18b9217733eb33d8bef0f194b145c022c63db
AUSTIK1 <austik55@gmail.com>
2023-06-19 20:25:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void){
 	int b; 
 	long int c;
 	long long int d;
-	float e;
+	float f;
 printf("size of char,%lu bytes\n",(unsigned long) sizeof(a));
 printf("size of int,%lu bytes\n",(unsigned long) sizeof(b));
 printf("size of long int,%lu bytes\n",(unsigned long) sizeofof(c));
 printf("size of long long int,%lu bytes\n",(unsigned long) sizeofof(d));
-printf("size of float,%lu bytes\n",(unsigned long) sizeofof(e));
+printf("size of float,%lu bytes\n",(unsigned long) sizeofof(f));
 return (0);
 }
