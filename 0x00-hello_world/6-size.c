453cb07be44826c31f6a95944413a22886fa0bbd
AUSTIK1 <austik55@gmail.com>
2023-06-20 05:16:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	char a;
-	int b; 
+	int b;
 	long int c;
 	long long int d;
 	float f;
@@ -17,4 +17,3 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 return (0);
 }
-
