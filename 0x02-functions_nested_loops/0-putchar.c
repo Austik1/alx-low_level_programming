b20f530aaffab9e0e29c93cc77aa6e488bdda19a
AUSTIK1 <austik55@gmail.com>
2023-07-02 04:47:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,10 @@
 	 * is written by mbah
 	 * Return: 0
 	 */
-int main(void)
-{
+
+
+	int main(void)
+	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,4 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+	}
+
