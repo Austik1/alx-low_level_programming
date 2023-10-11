3dc4d721956233f44fff9a7e37d18494cf4402c6
Austik1 <austik55@gmail.com>
2023-10-11 08:37:22 -0700

    README.md

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -8,7 +8,7 @@
  */
 void print_elem(int elem)
 {
-    printf("%d\n", elem);
+printf("%d\n", elem);
 }
 /**
  * print_elem_hex - prints an integer, in hexadecimal
@@ -18,7 +18,7 @@ void print_elem(int elem)
  */
 void print_elem_hex(int elem)
 {
-    printf("0x%x\n", elem);
+printf("0x%x\n", elem);
 }
 /**
  * main - check the code
@@ -27,8 +27,8 @@ void print_elem_hex(int elem)
  */
 int main(void)
 {
-    int array[5] = {0, 98, 402, 1024, 4096};
-    array_iterator(array, 5, &print_elem);
-    array_iterator(array, 5, &print_elem_hex);
-    return (0);
+int array[5] = {0, 98, 402, 1024, 4096};
+array_iterator(array, 5, &print_elem);
+array_iterator(array, 5, &print_elem_hex);
+return (0);
 }
